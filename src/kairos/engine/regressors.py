1c7514aadcf5d96f9d4e86674146994138552da5
Thomas J. Espel <thomas.espel@orange.fr>
2018-03-13 22:32:14 +0000

    Updated: regressors

--- src/kairos/engine/regressors.py
+++ src/kairos/engine/regressors.py
@@ -3,3 +3,28 @@
 
 # engine/utilityregressors.py
 # This file contains the functions used to interact with regressors
+
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.optimizers import SGD
+
+
+class linearRegressor:
+
+    def __init__(self, optimizer='sgd'):
+        inputs = Input(shape=(1,))
+        predictions = Dense(1, activation='linear')(inputs)
+        model = Model(inputs=inputs, outputs=predictions)
+        self.__model = Model(inputs=inputs, outputs=predictions)
+        sgd = SGD()
+        self.__model.compile(optimizer=sgd, loss='mse', metrics=['mse'])
+
+    def fit(self, input, output, shuffling=False):
+        self.__model(input, output, batch_size=1, epochs=30, shuffle=shuffling)
+        return 0
+
+    def predict(self, data):
+        return self.__model.predict(data)
+
+    def freeze(self):
+        return self.__model.to_json()
